1833886e65da7f9d91e1c23e3596691c62426d81
Gautam Chaskar <chaskargautam@gmail.com>
2022-01-25 19:52:44 +0530

    Assignment 1

--- Module1/force-app/main/default/lwc/calculator/calculator.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class Calculator extends LightningElement {
-    handleClick(){
-        this.dispatchEvent(new CustomEvent('clicked'));
-    }
-}
--- Module1/force-app/main/default/lwc/mainApplication/mainApplication.js
+++ Module1/force-app/main/default/lwc/mainApplication/mainApplication.js
@@ -1,15 +1,4 @@
 import { LightningElement } from 'lwc';
-exampleName = 'Sample Example';
 export default class MainApplication extends LightningElement {
-    eventName;
-    handleButtonClick(event){
-        this.eventName = 'Child Button Clicked';
-        this.exampleName = 'Handle Change From First Component'
-    }
-
-    handleButtonClick2(event){
-        this.eventName = '' + event.detail.fName + event.detail.lName;
-        this.exampleName = 'Handle Change From Second Component'
-
-    }
+    
 }
